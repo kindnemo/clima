b326f04f26c7a6208e53d9a9115386207f6d38d8
Naman Mishra <namanmishraepicstar@gmail.com>
2025-11-12 15:01:19 +0530

    Search bar now works the search bar is now working as intended looking for ways to add the suggestion menu

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import { fetchLocationData } from "./locationFetch";
 import { fetchWeatherData } from "./weatherFetch";
 import { updateData } from "./updateWeatherData";
 
+const searchButton = document.getElementById("search");
+console.log(searchButton);
+
 updateData();
 
+searchButton.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        // updateData(searchButton.value.trim());
+        updateData(searchButton.value.trim());
+        searchButton.value = "";
+    }
+});
 
 // fetchLocationData().then(data => {
 //     locationData = data;
